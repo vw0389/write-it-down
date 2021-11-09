7505d434202ab57981a1a33099a066a30532bd23
Victor Weinert <dev@vweinert.com>
2021-11-09 13:40:05 -0600

    added routes

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on ${PORT}!`);
+});
