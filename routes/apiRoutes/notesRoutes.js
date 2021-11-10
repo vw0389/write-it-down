aa1c7bf50ca9d1206a793746f64a41e0135f9fbf
Victor Weinert <dev@vweinert.com>
2021-11-10 02:37:11 -0600

    main functionality finishied, working on delete

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,11 +1,31 @@
-const path = require('path');
+const {notes} = require('../../data/db.json');
 const router = require('express').Router();
-
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+const {validateNote,createNote,deleteById,generateId} = require('../../lib/note');
+router.get('/notes', (req,res) =>  {
+    res.json(notes)
 });
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
+router.post('/notes', (req,res) =>  { 
+    // add id to body
+    req.body.id = generateId();
 
+    // Validate note
+    if (!validateNote(req.body)) {
+        res.status(400).send("note is not properly formatted");
+    } else {
+        // Creation of new note
+        const note = createNote(req.body,notes);
+        res.json(note);
+    }
+});
+router.delete('/notes/:id', (req,res) => {
+
+    let id = req.params.id;
+    
+    if (id < 0 || typeof id !== 'number') {
+        res.status(400).send("request not properly formatted");
+    } else {
+        deleteById(id,notes);
+        res.status(200).send("ok");
+    }
+});
 module.exports = router;
